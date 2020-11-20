1aba2de1b94b089a8e22e13f3cbfabba723ff963
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-11-20 21:59:32 +0200

    added an article and a source class

--- app/models/news.py
+++ app/models/news.py
@@ -1,1 +1,26 @@
-
+class Article:
+   '''
+   Article class to define News Objects
+   '''
+   def __init__(self,author,title,description,url,urltoImage,pusblishedAt,content):
+       self.author=author
+       self.title=title
+       self.description=description
+       self.url=url
+       self.urlImage=urlImage
+       self.publishedAt= publishedAt
+       self.content=content
+ 
+ 
+class Sources:
+   '''
+   Sources class to define Source Objects
+   '''
+ 
+   def __init__(self,id,name,description,url,category,country):
+       self.id=id
+       self.name=name
+       self.description=description
+       self.url=url
+       self.category=category
+       self.country=country
