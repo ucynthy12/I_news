9ed5c371acbeed0b7aaaeee316b1d5942a7ecd9d
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-11-20 22:00:19 +0200

    initialized the app

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from app import views
+# Initializing application
+app = Flask(__name__,instance_relative_config = True)
+ 
+#setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
