a69f03b9ff748bf99e404f536d7fa77545ed05be
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-11-22 07:24:28 +0200

    imported get aticle and get source function

--- app/views.py
+++ app/views.py
@@ -1,16 +1,20 @@
 
 from flask import render_template
 from app import app
+from .request import get_article,get_source
  
 # Views
 @app.route('/')
 def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    i_news = get_source()
+    print(i_news)
+   
+    top_headlines = get_article('top-headlines')
+    title = 'I_news - Your update on the latest news'
+    return render_template('index.html',title = title, headlines= top_headlines,news= i_news)
  
-   '''
-   View root page function that returns the index page and its data
-   '''
-   return render_template('index.html')
- 
-   #Getting top headlines news in the us
- 
-   top_headlines = get_article()
+  
