1082da13f5bb66e0723b8a833a9e734605a5d6d5
cynthia umutoniwabo <ucynthy12@gmail.com>
2020-11-22 07:25:11 +0200

    updated the config file

---
+++ app/config.py
@@ -0,0 +1,35 @@
+class Config:
+   '''
+   General configuration parent class
+   '''
+   NEWS_API_BASE_URL='https://newsapi.org/v2/{}?country=us&apiKey={}'
+   NEWS_SOURCE_LINK='https://newsapi.org/v2/sources?apiKey={}'
+   # NEWS_SOURCE_LINK='https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+
+  
+ 
+class ProdConfig(Config):
+   '''
+   Production  configuration child class
+ 
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   pass
+ 
+ 
+class DevConfig(Config):
+   '''
+   Development  configuration child class
+ 
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   NEWS_API_BASE_URL='https://newsapi.org/v2/{}?country=us&apiKey={}'
+   NEWS_SOURCE_LINK='https://newsapi.org/v2/sources?apiKey={}'
+   # NEWS_SOURCE_LINK='https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+
+
+
+ 
+   DEBUG = True
